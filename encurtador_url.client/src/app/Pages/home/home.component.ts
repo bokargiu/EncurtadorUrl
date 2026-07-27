b81e8423a1340c2752f15c6d19129beec7f02282
bokargiu <erickjstaack07@gmail.com>
2026-07-27 17:59:03 -0300

    Basic Home page foi criada

--- encurtador_url.client/src/app/Pages/home/home.component.ts
+++ encurtador_url.client/src/app/Pages/home/home.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  url: string | null = null;
 
+  sendUrl() {
+    this.url = 'teste';
+  }
 }
