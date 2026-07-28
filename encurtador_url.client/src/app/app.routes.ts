3196d0718641d15121493e64b4ecb25815acc43a
bokargiu <erickjstaack07@gmail.com>
2026-07-28 20:35:54 -0300

    app.component.spec foi apagado

--- encurtador_url.client/src/app/app.routes.ts
+++ encurtador_url.client/src/app/app.routes.ts
@@ -4,8 +4,8 @@ import { HomeComponent } from './Pages/home/home.component';
 import { RedirectComponent } from './Pages/redirect/redirect.component';
 
 export const routes: Routes = [
-  {path: '', component: HomeComponent, pathMatch: 'full'},
-  {path: 'red/:code', component: RedirectComponent},
+  {path: '', component: HomeComponent, title: 'Encurtador de Links', pathMatch: 'full'},
+  {path: 'red/:code', title: 'Redirecionando', component: RedirectComponent},
   {path: '**', redirectTo: '', pathMatch: 'full'}
 ];
 
